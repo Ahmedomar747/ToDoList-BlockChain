2e29367a2cb344a0c03a454af95b11320230f63a
ahmedomar197 <ahmedomar747@gmail.com>
2020-03-26 22:09:08 +0200

    Defined Task in Contract

--- contracts/ToDooList.sol
+++ contracts/ToDooList.sol
@@ -3,4 +3,20 @@ pragma solidity ^0.5.0;
 
 contract ToDoList {
     uint public TaskCount = 0;
+    struct Task{
+        uint id;
+        string title;
+        string content;
+        bool isCompleted;
+    }
+    mapping(uint => Task) public Tasks;
+
+    constructor() public {
+        createTask("WorkOut","Full body workout from 6PM to 8 PM.");
+    }
+
+    function createTask(string memory _title, string memory _content) public {
+        TaskCount ++;
+        Tasks[TaskCount] = Task(TaskCount, _title, _content, false);
+    }
 }
